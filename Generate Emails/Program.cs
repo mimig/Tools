a9db46583f128ff1b86d7d4bcf44cb3d79d6c33a
Mimi Guha <Mimi.Guha@ipreo.com>
2018-03-03 22:24:30 -0500

    Tool to generate emails

---
+++ Generate Emails/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Generate_Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = string.Empty;
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Please enter a path.");
+                path = Console.ReadLine();
+            }
+         
+            StringBuilder appendText = new StringBuilder();
+       
+            for (int i = 0; i < 20; i++)
+            {
+                appendText.Append(string.Format("testbu_{0}@test-prism.com", DateTime.UtcNow.Ticks));
+                appendText.AppendLine();
+            }
+
+            File.WriteAllText(path, appendText.ToString());
+         
+            // Open the file to read from.
+            string readText = File.ReadAllText(path);
+            Console.WriteLine(readText);
+        }
+    }
+}
